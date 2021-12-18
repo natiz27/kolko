46a50b1aaa6a5c4824c1a42e64b9780580a1928e
natiz27 <starszakn@gmail.com>
2021-12-18 10:31:36 +0100

    plansza do gry

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-#%% Gra kółko i krzyżyk
+# Gra kółko i krzyżyk
 
-PlanszaDoGry = {}
+PlanszaDoGry = {'7':' ','8':' ','9':' ',
+                '4':' ','5':' ','6':' ',
+                '1':' ','2':' ','3':' '}
+
+klawiszeGry=[]
+
+for key in PlanszaDoGry:
+    klawiszeGry.append(key)
+    #print(klawiszeGry)
+#print(klawiszeGry)
+
+
+def drukujPlansze(pole):
+    print(f"{pole['7']}|{pole['8']}|{pole['9']}")
+    print('-+-+-')
+    print(f"{pole['4']}|{pole['5']}|{pole['6']}")
+    print('-+-+-')
+    print(f"{pole['1']}|{pole['2']}|{pole['3']}")
+
+drukujPlansze(PlanszaDoGry)
