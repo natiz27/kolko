393dd4a4ab7c28c5be9a903108faaf522c483092
natiz27 <you@example.com>
2021-12-18 23:26:31 +0100

    zmiana_PC

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Gra kółko i krzyżyk
+#%% Gra kółko i krzyżyk
 
 PlanszaDoGry = {'7':' ','8':' ','9':' ',
                 '4':' ','5':' ','6':' ',
@@ -19,4 +19,93 @@ def drukujPlansze(pole):
     print('-+-+-')
     print(f"{pole['1']}|{pole['2']}|{pole['3']}")
 
-drukujPlansze(PlanszaDoGry)
+# drukujPlansze(PlanszaDoGry)
+
+def gra():
+    gracz ='X'
+    licznik=0 #bedzie zliaczać i sprawdzać, czy ktoś wygrał
+
+    for i in range(10):
+        drukujPlansze(PlanszaDoGry)
+        
+        move=input(f'To jest ruch, [{gracz}. Wybierz gdzie chcesz postawić znak!')
+
+        if PlanszaDoGry[move]==' ':
+            PlanszaDoGry[move]=gracz
+            licznik += 1
+        else:
+            print("Miejsce jest już zajęte!!!\n,Wstaw swój znak gdzieś indziej!")
+            continue
+
+        if licznik >= 5:
+#sprawdzenie wygranych poziomych
+            if PlanszaDoGry['7']==PlanszaDoGry['8']==PlanszaDoGry['9'] != ' ':
+                drukujPlansze(PlanszaDoGry)
+                print('\nKONIEC GRY!\n')
+                print(f'Wygrał gracz: {gracz}')
+                break
+
+            elif PlanszaDoGry['4']==PlanszaDoGry['5']==PlanszaDoGry['6'] != ' ':
+                drukujPlansze(PlanszaDoGry)
+                print('\nKONIEC GRY!\n')
+                print(f'Wygrał gracz: {gracz}')
+                break
+
+            elif PlanszaDoGry['1']==PlanszaDoGry['2']==PlanszaDoGry['3'] != ' ':
+                drukujPlansze(PlanszaDoGry)
+                print('\nKONIEC GRY!\n')
+                print(f'Wygrał gracz: {gracz}')
+                break 
+
+#sprawdzenie wygrancyh pionowych
+            elif PlanszaDoGry['7']==PlanszaDoGry['4']==PlanszaDoGry['1'] != ' ':
+                drukujPlansze(PlanszaDoGry)
+                print('\nKONIEC GRY!\n')
+                print(f'Wygrał gracz: {gracz}')
+                break
+
+            elif PlanszaDoGry['8']==PlanszaDoGry['5']==PlanszaDoGry['2'] != ' ':
+                drukujPlansze(PlanszaDoGry)
+                print('\nKONIEC GRY!\n')
+                print(f'Wygrał gracz: {gracz}')
+                break
+
+            elif PlanszaDoGry['9']==PlanszaDoGry['6']==PlanszaDoGry['3'] != ' ':
+                drukujPlansze(PlanszaDoGry)
+                print('\nKONIEC GRY!\n')
+                print(f'Wygrał gracz: {gracz}')
+                break 
+
+#sprawdzenie wygranych przekątncyh
+            elif PlanszaDoGry['7']==PlanszaDoGry['5']==PlanszaDoGry['3'] != ' ':
+                drukujPlansze(PlanszaDoGry)
+                print('\nKONIEC GRY!\n')
+                print(f'Wygrał gracz: {gracz}')
+                break
+
+            elif PlanszaDoGry['1']==PlanszaDoGry['5']==PlanszaDoGry['9'] != ' ':
+                drukujPlansze(PlanszaDoGry)
+                print('\nKONIEC GRY!\n')
+                print(f'Wygrał gracz: {gracz}')
+                break 
+
+#sprawdzenie licznika, czy gra się zakończyła
+            if licznik == 9:
+                print('\nKONIEC GRY!!!\n')
+                print('\nJest remis!!!\n')
+        
+        if gracz == 'X':
+            gracz = 'O'
+        else:
+            gracz='X'
+    
+    restart=input('czy chcesz zagrać ponownie?/(t/n)')
+    if restart=='t' or restart=='T':
+        for key in klawiszeGry:
+            PlanszaDoGry[key]=' '
+        gra()
+            
+#superFunkcja - stosuje się 2 podkreślenia - jeżeli nazwa modułu jest nazwana main to uruchamiana jest gra - wywołanie - spakowanie do pakietu i wywołanie gry
+if __name__=='__main__':
+    gra()
+
